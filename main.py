c11b94f3559c3982ad6a94233d56be7d9c444a43
MrSquirel <99865132+MrSquirel@users.noreply.github.com>
2022-04-07 18:30:10 +1000

    Add files via upload

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import pyb
 uart = UART(6, 115200, stop=1)
 message = "123$"
 inn = ''
-serv_deg = ADC("X4")
+serv_deg = ADC("X1")
 
 pic = Pin('Y10', Pin.OUT_PP)
 # p_out.high()
@@ -19,25 +19,63 @@ MS = Pin('X10')
 tim = Timer(4, freq = 10000)
 ch = tim.channel(2, Timer.PWM, pin = MS)
 
+S1 = Pin('X7', Pin.OUT_PP)
+S2 = Pin('X8', Pin.OUT_PP)
+SS = Pin('X9')
+tim = Timer(4, freq = 10000)
+chS = tim.channel(1, Timer.PWM, pin = SS)
+
 def Motor(sp):
     global ch, M1, M2
     if sp < 0:
         M1.low()
         M2.high()
         sp = -sp
-        print(sp)
+
     else:
         M2.low()
         M1.high()
     ch.pulse_width_percent(sp)
 
+eold = 0
+
+def Serv(deg): # управление сервой(пд регулятор)
+    global eold, S1, S2, chS
+    if deg < 450:
+        deg = 450
+    if deg > 2990:
+        deg = 2990
+    e = serv_deg.read() - deg # ошибка
+    u = e * 0.1 + (e - eold)*0.3 # скорость
+    if u < 0:
+        S1.low()
+        S2.high()
+        u = -u
+    else:
+        S2.low()
+        S1.high()
+    if u > 100:
+        u = 100
+    if u < 5:
+        u = 15
+    if -50< e < 50:
+        u = 0
+        S1.low()
+        S2.low()
+    chS.pulse_width_percent(u)
+    eold = e
+
 flag_start = True
 speed = 0
 rul = 0
-
+flag =1
 
 
 while True:
+    if flag == 1:
+        Motor(0)
+        Serv(1800)
+    #print(serv_deg.read()) # 780 , 3027, 2112
     if uart.any():
         a = chr(uart.readchar())
         if a != '$':
@@ -51,6 +89,7 @@ while True:
                     pic.high()
                     delay(500)
                     pic.low()
+                    flag = 0
             else:
                 message = 'B=' + str(button.value()) + '$'
                 try:
@@ -58,7 +97,8 @@ while True:
                         speed = int(inn[:3])-200
                         rul = int(inn[3:])-1000
                         print(speed, rul)
-                        Motor(30)
+                        Motor(speed)
+                        Serv(rul)
                 except ValueError:
                     print("err")
 
